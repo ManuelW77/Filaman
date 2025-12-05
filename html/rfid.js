1ab6e330424ed774cc9e6a096480dba47ecc59a3
Gunny Patel <patel.gunny@gmail.com>
2025-12-04 23:19:08 -0500

    Support WSS when using Filaman over HTTPS

--- html/rfid.js
+++ html/rfid.js
@@ -59,7 +59,8 @@ function initWebSocket() {
     }
 
     try {
-        socket = new WebSocket('ws://' + window.location.host + '/ws');
+        const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+        socket = new WebSocket(protocol + '//' + window.location.host + '/ws');
         
         socket.onopen = function() {
             isConnected = true;
